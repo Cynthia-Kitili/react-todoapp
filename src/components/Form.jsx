fddb0cef37166661478ed755476ed1300eeed2f3
KEN20971 <ckitili@kcbgroup.com>
2025-02-10 09:41:00 +0300

    update delete

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import styles from "./form.module.css";
 
-export default function Form({todos,setTodos}) {
+export default function Form({ todos, setTodos }) {
     const [todo, setTodo] = useState("")
     function handleSubmit(e) {
         e.preventDefault(); // prevents default submission & refresh
@@ -9,9 +10,16 @@ export default function Form({todos,setTodos}) {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input onChange={(e) => setTodo(e.target.value)} value={todo} type="text" />
-            <button type="submit">Add</button>
+        <form className={styles.todoform} onSubmit={handleSubmit}>
+            <div className={styles.inputContainer}>
+                <input className={styles.modernInput}
+                    onChange={(e) => setTodo(e.target.value)}
+                    value={todo} type="text"
+                    placeholder="Enter todo item....." />
+                <button className={styles.modernButton}
+                    type="submit">Add</button>
+            </div>
+
         </form>
     )
 }
